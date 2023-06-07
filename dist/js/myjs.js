4ece1fb630064a80188daba0e50cdf9157648b02
hybf1212 <hybf1212@naver.com>
2023-06-07 18:09:56 +0900

    포트폴리오 준비

---
+++ dist/js/myjs.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('#banner_1 .swiper', {
+
+    effect: "fade",
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+      },
+  
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+
+  });
